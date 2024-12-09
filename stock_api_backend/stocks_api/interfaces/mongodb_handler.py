49b1f803373da41aa86823b9951dc925b8d381f4
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-09 17:45:37 -0500

    eliminate issue with duplicate entries with upsert

--- stock_api_backend/stocks_api/interfaces/mongodb_handler.py
+++ stock_api_backend/stocks_api/interfaces/mongodb_handler.py
@@ -8,7 +8,9 @@ db=client['stock-backend']
 collection=db['stock-backend-collection']
 
 def save_to_mongo(data):
-    collection.insert_one(data)
+    #collection.insert_one(data)
+    query={'ticker':data['ticker']}
+    collection.update_one(query,{'$set':data},upsert=True)
 
 def fetch_from_mongo():
     return list(collection.find({},{'_id':0}))
