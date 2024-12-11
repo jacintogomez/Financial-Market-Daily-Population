44919420d002ba5e2af0e52f3a552698220dd9ed
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-11 17:42:39 -0500

    adding parameters in response returned

--- stock_api_backend/stocks_api/utils.py
+++ stock_api_backend/stocks_api/utils.py
@@ -3,38 +3,45 @@ from typing import Generic, Optional, TypeVar
 
 T=TypeVar('T')
 
-class FMPData:
-    def __init__(self,data=None):
-        self.price=data['Stock Price']
-    def __str__(self):
-        return f'Price: {self.price}'
-    def to_dict(self):
-        return { 'price': self.price if self.price else None }
-
-class EODHDData:
-    def __init__(self,data=None):
-        self.price=data['close'] if data else None
-    def to_dict(self):
-        return { 'price': self.price if self.price else None }
+class StockData:
+    def __init__(self,ticker):
+        self.ticker=ticker
+        self.price=0
+        self.day_high=0
+        self.day_low=0
+        self.open_price=0
+        self.change=0
+        self.percent_change=0
+        self.volume=0
+        self.market_cap=0
+        self.pe_ratio=0
+        self.dividend_yield=0
 
 class APIResponse:
-    def __init__(self,ticker,FMP=None,EODHD=None):
+    def __init__(self,ticker,provider=None,data=None):
         self.ticker=ticker
         self.timestamp=datetime.now(timezone.utc).isoformat()
-        self.FMP=FMP
-        self.EODHD=EODHD
+        self.provider=provider
+        self.data=data
     def __str__(self):
         return self.ticker
     def to_dict(self):
         return {
             'ticker': self.ticker,
             'timestamp': self.timestamp,
-            'FMP': {
-                'price': self.FMP.price if self.FMP else None
+            'provider': self.provider,
+            'data': {
+                'price': self.data.price if self.data.price else None,
+                'day_high': self.data.day_high if self.data.day_low else None,
+                'day_low': self.data.day_low if self.data.day_high else None,
+                'open_price': self.data.open_price if self.data.open_price else None,
+                'change': self.data.change if self.data.change else None,
+                'percent_change': self.data.percent_change if self.data.percent_change else None,
+                'volume': self.data.volume if self.data.volume else None,
+                'market_cap': self.data.market_cap if self.data.market_cap else None,
+                'pe_ratio': self.data.pe_ratio if self.data.pe_ratio else None,
+                'dividend_yield': self.data.dividend_yield if self.data.dividend_yield else None,
             },
-            'EODHD': {
-                'price': self.EODHD.price if self.EODHD else None
-            }
         }
 
 class ErrorDetails:
--- stock_api_backend/stocks_api/views.py
+++ stock_api_backend/stocks_api/views.py
@@ -4,29 +4,36 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .use_cases.get_stock_data import fetch_stock_data_fmp,fetch_stock_data_eodhd
 from .interfaces.mongodb_handler import save_to_mongo,fetch_from_mongo
-from .utils import APIResponse,FMPData,EODHDData
+from .utils import APIResponse
 from bson import ObjectId
 
 api_url='https://localhost/stocks'
+api_functions=[fetch_stock_data_fmp]
 
 @api_view(['GET'])
 def get_stock_data(request,symbol):
-
+    """Takes in stock symbol and produces a dictionary with the information
+    in the StockData class, also saving to the database"""
     # TODO change the below line to accept all APIs
-    fmp_response_code,fmp_response=fetch_stock_data_fmp(symbol)
-    eod_response_code,eod_response=fetch_stock_data_eodhd(symbol)
 
-    fmp_data=FMPData(fmp_response) if fmp_response_code==200 else None
-    eod_data=EODHDData(eod_response) if eod_response_code==200 else None
-    stock_data=APIResponse(symbol,fmp_data,eod_data)
-    print(stock_data.to_dict())
-    stocks_dictionary=stock_data.to_dict()
+    # Iterate over every API option to find the stock data
+    # Once it is found on a provider API, stop and return those results
+    for api in api_functions:
+        response_code,retrieved_data,api_provider=api(symbol)
+        if response_code==200 and retrieved_data is not None:
+            stock_data=APIResponse(api_provider,retrieved_data)
+            print(stock_data.to_dict())
+            stocks_dictionary=stock_data.to_dict()
 
-    save_to_mongo(stocks_dictionary)
-    return Response(stocks_dictionary)
+            save_to_mongo(stocks_dictionary)
+            return Response(stocks_dictionary)
+
+    # If the stock is not retrievable by any API (or doesn't exist)
+    return Response({'message':'Stock Data Not Found'})
 
 @api_view(['GET'])
 def get_all_stocks(request):
+    """Returns a list of all stocks in the database"""
     stocks=fetch_from_mongo()
     for stock in stocks:
         # MongoDB specific: need to convert the mandatory _id field from ObjectId to string
