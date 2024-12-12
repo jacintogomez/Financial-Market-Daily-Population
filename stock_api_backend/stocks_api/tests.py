6e7c30e4f041e915c8b3f05981ec021ffeb22087
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-12 13:14:43 -0500

    Addition of unit tests

--- stock_api_backend/stocks_api/tests.py
+++ stock_api_backend/stocks_api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .utils import StockData,APIResponse
+class Tests(TestCase):
+    def test_stock_data_initialization(self):
+        stock=StockData('AAPL')
+        assert stock.ticker=='AAPL'
+        assert stock.price==0
+        assert stock.convert_data_to_dict()==stock.__dict__
 
-# Create your tests here.
+    def test_api_response_creation(self):
+        stock_data=StockData('AAPL')
+        message='Retrieved data successfully'
+        response=APIResponse(200,message,stock_data)
+        response_dict=response.to_dict()
+        assert response_dict['status_code']==200
+        assert response_dict['message']==message
+
+    def test_fetch_stock_data_fmp_success(self):
+        pass
+
+    def test_fetch_stock_data_fmp_fail(self):
+        pass
