697d498babab4716a02cc9d208d49c6472657ba6
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-13 10:05:29 -0500

    Addition of langchain bot to create a summary based on given context

--- stock_api_backend/stocks_api/ai/ai_views.py
+++ stock_api_backend/stocks_api/ai/ai_views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
 from dotenv import load_dotenv
+from decouple import config
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import os
+import requests
+import json
 load_dotenv()
 
-fundamental_url=''
 api_key=os.getenv('OPENAI_API_KEY')
-
+fmp_key=config('FMP_API_KEY')
+fundamental_url='https://financialmodelingprep.com/api/v3/quotes/index?'+fmp_key
 
 def ai_summary(request):
     llm=ChatOpenAI()
-    #task='Based on the received json data, please give a general summary of what happened in the market (so far) today'
-    task='what is 2+2?'
-    prompt = ChatPromptTemplate.from_template(
-        "You are an elementary school kid. {input}"
-    )
+    fundamentals=requests.get(fundamental_url).json()
+    formatted_data=json.dumps(fundamentals,indent=4)
+    task=f'Based on the received json data, please give a general summary of the market so far today\n{formatted_data}'
+
+    prompt = ChatPromptTemplate.from_template("You are an expert financial analyst. {input}")
 
     output_parser=StrOutputParser()
     chain= prompt | llm | output_parser
