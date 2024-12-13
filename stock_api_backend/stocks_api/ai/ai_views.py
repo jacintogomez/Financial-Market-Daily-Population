08d59a30b225320aa05bc1ac1128fb5175830169
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-13 16:31:53 -0500

    Adding new AI prompts

--- stock_api_backend/stocks_api/ai/ai_views.py
+++ stock_api_backend/stocks_api/ai/ai_views.py
@@ -11,13 +11,18 @@ load_dotenv()
 
 api_key=os.getenv('OPENAI_API_KEY')
 fmp_key=config('FMP_API_KEY')
-fundamental_url='https://financialmodelingprep.com/api/v3/quotes/index?apikey='+fmp_key
+fundamental_url='https://financialmodelingprep.com/api/v3/quotes/index?apikey='
+news_url='https://financialmodelingprep.com/api/v3/stock_news?tickers=AAPL,FB&page=3&from=2024-01-01&to=2024-03-01&apikey='
+fx_url=''
+acquisitions_url='https://financialmodelingprep.com/api/v4/mergers-acquisitions-rss-feed?page=0&apikey='
 
 def ai_summary(request):
     llm=ChatOpenAI()
-    fundamentals=requests.get(fundamental_url).json()[:10]
+    fundamentals=requests.get(fx_url+fmp_key).json()
     formatted_data=json.dumps(fundamentals,indent=4)
-    task=f'Based on the received json data, please give a general summary of the market so far today\n{formatted_data}'
+    #task=f'Based on the received json data, please give a general summary of the market so far today\n{formatted_data}'
+    #task=f'Please give a general summary of the merges and acquisitions this week\n{formatted_data}'
+    task=f'Please give a general summary of the relevant financial news stories today\n{formatted_data}'
 
     prompt = ChatPromptTemplate.from_template("You are an expert financial analyst. {input}")
 
