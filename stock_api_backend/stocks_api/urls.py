22e0b41dc1a41e27d028b3d6721a7096c56aa2f1
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-16 15:24:08 -0500

    Redis & Celery setup to handle asynchronous tasks

--- stock_api_backend/stocks_api/urls.py
+++ stock_api_backend/stocks_api/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('fundamentals/',get_fundamentals),
     #path('market_exchange_data/<str:market_ticker>',get_assets_under_market),
     path('market_exchange/',get_market_exchange_data),
-    path('clear_collections/',clear_test_collections),
+    path('clear_collections/',clear_test_collections), # needs to be deleted at some point
 ]
