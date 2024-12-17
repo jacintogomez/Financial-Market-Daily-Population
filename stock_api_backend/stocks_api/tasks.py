1409fe0b25d3609a223d5c775758f54fb3ce733e
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-17 13:16:59 -0500

    Kafka setup

--- stock_api_backend/stocks_api/tasks.py
+++ stock_api_backend/stocks_api/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from .kafka_producer import send_message
 from .use_cases.get_stock_data import fetch_all_symbols_from_market,fetch_market_exchange_data
 from .interfaces.mongodb_handler import save_asset_to_mongo,save_market_to_mongo
 
@@ -14,8 +15,10 @@ def populate_market_stocks(self,market_ticker):
 
 @shared_task
 def async_market_population():
+    print('starting population task')
     code,markets=fetch_market_exchange_data()
     for market in markets[:3]:
         save_market_to_mongo(market)
         populate_market_stocks.delay(market['Code'])
+    send_message('market_data','population_complete','Database population bas been completed successfully')
     return code
