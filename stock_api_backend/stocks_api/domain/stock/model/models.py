6237a8e6c784732ea62131e2c3a1cff254e7736e
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-20 12:01:58 -0500

    Added in models for IPO and Stocks categories

---
+++ stock_api_backend/stocks_api/domain/stock/model/models.py
@@ -0,0 +1,27 @@
+from mongoengine import Document
+from mongoengine import StringField, DictField
+
+class Stock(Document):
+    symbol=StringField(max_length=20,required=True,unique=True)
+    data=DictField()
+    provider=StringField(max_length=20)
+    meta={
+        'collection':'stocks',
+        'indexes':[
+            {'fields':['symbol'],'unique':True},
+        ],
+        'index_background':True,
+    }
+    def to_dict(self):
+        return {
+            'symbol': self.symbol,
+            'data': self.data or None,
+            'provider': self.provider or None,
+        }
+    @classmethod
+    def upsert_stock(cls,symbol,new_data):
+        return cls.objects(symbol=symbol).modify(
+            upsert=True,
+            new=True,
+            set__data=new_data,
+        )
