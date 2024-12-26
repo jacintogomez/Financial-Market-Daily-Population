51938787da366d8f43afc75de55b8241cfef8332
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-26 16:24:28 -0500

    Update using chord to make a callback to the webhook after the population is finished

--- stock_api_backend/stocks_api/tasks.py
+++ stock_api_backend/stocks_api/tasks.py
@@ -1,4 +1,4 @@
-from celery import shared_task,group
+from celery import shared_task,group,chord
 from celery.result import GroupResult
 from .use_cases.get_stock_data import fetch_all_symbols_from_market,fetch_market_exchange_data
 from .interfaces.mongodb_handler import save_asset_to_mongo,save_market_to_mongo
@@ -6,12 +6,28 @@ from .webhook_handler import WebhookTask
 from .domain.apiresponse.model.models import APIResponse
 from django.http import JsonResponse
 
+@shared_task
+def webhook_push(results,task_id):
+    success=all(result.get('code')==200 for result in results)
+    message='All markets populated successfully' if success else 'Failure in market population'
+    webhook=WebhookTask()
+    if success:
+        webhook.on_success({'message':message},task_id,[],{})
+    else:
+        webhook.on_failure(Exception(message),task_id,[],{},None)
+    print('time to send a message')
+    # return {
+    #     'success': success,
+    #     'message': message,
+    # }
+
 @shared_task(bind=True,max_retries=3)
 def populate_market_stocks(self,market_ticker):
     try:
         code,symbols=fetch_all_symbols_from_market(market_ticker)
         symbol_errors=[]
         for symbol in symbols[:5]:
+            print('populating stock',symbol)
             try:
                 save_asset_to_mongo(symbol,market_ticker)
             except Exception as e:
@@ -40,13 +56,15 @@ def async_market_population(self):
                 market_errors.append(f'Failed to save market {market}: {str(e)}')
         if market_errors:
             raise Exception(f'Error saving market data: {market_errors}')
-        market_population_tasks=group(populate_market_stocks.s(market['Code']) for market in markets[:5])
-        result=market_population_tasks.apply_async()
-        group_result=GroupResult.restore(result.id)
-        group_result.get()
+        # market_population_tasks=group(populate_market_stocks.s(market['Code']) for market in markets[:5])
+        # result=group(market_population_tasks).apply_async()
+        # result.get()
+        market_population_tasks=[populate_market_stocks.s(market['Code']) for market in markets[:5]]
+        callback=webhook_push.s(task_id=self.request.id)
+        chord(market_population_tasks)(callback)
         return {
             'code': code,
-            'message': f'All markets saved successfully',
+            'message': f'Database population started.',
         }
     except Exception as e:
         raise Exception(f'Error populating database with market data: {str(e)}')
