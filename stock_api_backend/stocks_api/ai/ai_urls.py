5250c60948a5a890f8e58336257846c9f09877ca
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2025-01-24 12:46:22 -0500

    Pass company ticker as url parameter

--- stock_api_backend/stocks_api/ai/ai_urls.py
+++ stock_api_backend/stocks_api/ai/ai_urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import ai_views
 
 urlpatterns=[
-    path("",ai_views.ai_summary,name="ai_summary"),
+    path("<stock_symbol>",ai_views.ai_summary,name="ai_summary"),
 ]
--- stock_api_backend/stocks_api/ai/ai_views.py
+++ stock_api_backend/stocks_api/ai/ai_views.py
@@ -11,18 +11,19 @@ load_dotenv()
 
 api_key=os.getenv('OPENAI_API_KEY')
 fmp_key=config('FMP_API_KEY')
-fundamental_url='https://financialmodelingprep.com/api/v3/quotes/index?apikey='
+fundamentals_url='https://financialmodelingprep.com/api/v3/quotes/index?apikey='
 news_url='https://financialmodelingprep.com/api/v3/stock_news?tickers=AAPL,FB&page=3&from=2024-01-01&to=2024-03-01&apikey='
 fx_url=''
 acquisitions_url='https://financialmodelingprep.com/api/v4/mergers-acquisitions-rss-feed?page=0&apikey='
 
-def ai_summary(request):
+def ai_summary(request,stock_symbol):
     llm=ChatOpenAI()
-    fundamentals=requests.get(fx_url+fmp_key).json()
-    formatted_data=json.dumps(fundamentals,indent=4)
+    url=f'https://financialmodelingprep.com/api/v3/stock_news?tickers={stock_symbol}&apikey={fmp_key}'
+    news_stories=requests.get(url).json()
+    formatted_data=json.dumps(news_stories,indent=4)
     #task=f'Based on the received json data, please give a general summary of the market so far today\n{formatted_data}'
     #task=f'Please give a general summary of the merges and acquisitions this week\n{formatted_data}'
-    task=f'Please give a general summary of the relevant financial news stories today\n{formatted_data}'
+    task=f'Please give a general summary of the relevant financial news stories for {stock_symbol}\n{formatted_data}'
 
     prompt = ChatPromptTemplate.from_template("You are an expert financial analyst. {input}")
 
